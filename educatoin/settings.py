cdbc75f00565293729aa4200afd034db2d97a5bb
Iamnikhil <51186856+nikhilkotiya@users.noreply.github.com>
2021-03-11 18:23:45 +0530

    Update settings.py

--- educatoin/settings.py
+++ educatoin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o%dear2nwh-9q4w2!8no6tkbr0i98mm%00k8*%r#7p&_=l65z('
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
